4253a0873b806a596849eef5b1f9a9029dfaf286
P. Envall <petter.envall@gmail.com>
2023-02-19 17:20:27 +0100

    chore(update): update version of dependency "astro" to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 const isProd = "production" === process.env.NODE_ENV;
 
 const outDir = isProd ? "docs" : "docs-local";
+
+console.info("Astri building", { isProd, outDir });
+
 export default defineConfig({
     outDir,
     server: {
-    port: 3003,
-  },
+        port: 3003,
+    },
 });
